65542490ae4ae9b33d3d4648024cf11ef2417096
Sanket <sanketbarot3901@gmail.com>
2026-05-06 16:05:11 +0530

    download all

---
+++ js/adsense.js
@@ -0,0 +1,12 @@
+// js/adsense.js ma aa rite use karo:
+
+const ADSENSE_CONFIG = {
+    publisherId: 'ca-pub-8473875587893208',
+    slots: {
+        topBanner:    'XXXXXXXXXX',  // AdSense ma banaavo
+        middleBanner: 'XXXXXXXXXX',  // AdSense ma banaavo
+        bottomBanner: 'XXXXXXXXXX',  // AdSense ma banaavo
+        sidebar:      'XXXXXXXXXX',  // AdSense ma banaavo
+        inArticle:    'XXXXXXXXXX',  // AdSense ma banaavo
+    }
+};
